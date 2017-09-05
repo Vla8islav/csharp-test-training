4b851510844f741b16ae77fecf0619d8452f2988
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2017-09-05 21:12:05 +0300

    Created first sample test using Firefox Selenium IDE.

--- csharp-test-training/csharp-test-training/UnitTest1.cs
+++ csharp-test-training/csharp-test-training/UnitTest1.cs
@@ -1,13 +1,110 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace addressbook_web_tests
 {
-    [TestClass]
-    public class UnitTest1
+    [TestFixture]
+    public class Untitled
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
         {
+            FirefoxBinary firefox = new FirefoxBinary(@"C:\Program Files (x86)\Mozilla Firefox_45_9_0_esr\firefox.exe");
+            driver = new FirefoxDriver(firefox,
+                new FirefoxProfile());
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/group.php");
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys("e3Zr14G14MoXkQ0TS8t8");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            driver.FindElement(By.LinkText("groups")).Click();
+            driver.FindElement(By.Name("new")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys("Some new goup");
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys("Some group header");
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys("Некоторый русский текст для разнообразия.");
+            driver.FindElement(By.Name("submit")).Click();
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
         }
     }
 }
