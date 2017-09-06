683f6c97937aeedd5da4c61c2d55615612952b05
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2017-09-06 23:52:33 +0300

    Removed unused methods

--- csharp-test-training/csharp-test-training/UnitTest1.cs
+++ csharp-test-training/csharp-test-training/UnitTest1.cs
@@ -102,54 +102,6 @@ namespace addressbook_web_tests
         {
             driver.Navigate().GoToUrl(baseURL + "addressbook/");
         }
-
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
     }
 
 }
