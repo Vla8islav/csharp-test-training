05f490082b16451e189094170b68889b0255aa14
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2017-09-07 07:05:21 +0300

    Created contact creation test

---
+++ csharp-test-training/csharp-test-training/ContactData.cs
@@ -0,0 +1,35 @@
+﻿namespace addressbook_web_tests
+{
+    public class ContactData
+    {
+        public ContactData(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public string Photo { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+        public string Home { get; set; }
+        public string Mobile { get; set; }
+        public string Work { get; set; }
+        public string Fax { get; set; }
+        public string EMail { get; set; }
+        public string EMail2 { get; set; }
+        public string EMail3 { get; set; }
+        public string Homepage { get; set; }
+        public string Birthday { get; set; }
+        public string Anniversary { get; set; }
+        public string Group { get; set; }
+        public string Secondary { get; set; }
+        public string SecondaryAddress { get; set; }
+        public string SecondaryHome { get; set; }
+        public string Notes { get; set; }
+    }
+}
--- csharp-test-training/csharp-test-training/CreateContactTest.cs
+++ csharp-test-training/csharp-test-training/CreateContactTest.cs
@@ -1,7 +1,99 @@
-﻿namespace addressbook_web_tests
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace addressbook_web_tests
 {
     public class CreateContactTest
     {
-        
+        private IWebDriver _driver;
+        private StringBuilder _verificationErrors;
+        private string _baseUrl;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxDriverFactory driverFactory = new FirefoxDriverFactory();
+            TestingEnvironment environment = new TestingEnvironment();
+
+            _driver = driverFactory.GetFirefoxDriver();
+            _baseUrl = environment.BaseUrl;
+            _verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                _driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", _verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTest()
+        {
+            OpenMainPage();
+            FillLoginForm(new AccountData("admin", "e3Zr14G14MoXkQ0TS8t8"));
+            SubmitLoginForm();
+
+            GoToContactCreationPage();
+            ContactData data = new ContactData("TestName");
+            data.MiddleName = "TestMiddleName";
+            data.LastName = "TestLastName";
+            FillContactForm(data);
+            SubmitContactData();
+            Logout();
+
+        }
+
+        private void SubmitContactData()
+        {
+            _driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void GoToContactCreationPage()
+        {
+            _driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Logout()
+        {
+            _driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void FillContactForm(ContactData data)
+        {
+            _driver.FindElement(By.Name("firstname")).Clear();
+            _driver.FindElement(By.Name("firstname")).SendKeys(data.FirstName);
+            _driver.FindElement(By.Name("middlename")).Clear();
+            _driver.FindElement(By.Name("middlename")).SendKeys(data.MiddleName);
+            _driver.FindElement(By.Name("lastname")).Clear();
+            _driver.FindElement(By.Name("lastname")).SendKeys(data.LastName);
+        }
+
+        private void SubmitLoginForm()
+        {
+            _driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void FillLoginForm(AccountData data)
+        {
+            _driver.FindElement(By.Name("user")).Clear();
+            _driver.FindElement(By.Name("user")).SendKeys(data.Login);
+            _driver.FindElement(By.Name("pass")).Clear();
+            _driver.FindElement(By.Name("pass")).SendKeys(data.Password);
+        }
+
+        private void OpenMainPage()
+        {
+            _driver.Navigate().GoToUrl(_baseUrl + "addressbook/");
+        }
     }
 }
