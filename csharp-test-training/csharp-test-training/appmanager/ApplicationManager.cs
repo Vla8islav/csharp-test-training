ea957e0fdec37291c1c3f28276d1eab75604c76e
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2017-09-10 00:54:29 +0300

    Created auto properties and changed the application a bit

--- csharp-test-training/csharp-test-training/appmanager/ApplicationManager.cs
+++ csharp-test-training/csharp-test-training/appmanager/ApplicationManager.cs
@@ -5,38 +5,33 @@ namespace addressbook_web_tests
 {
     public class ApplicationManager
     {
-        protected IWebDriver _driver;
-        protected AccountFactory _accountFactory;
         protected TestingEnvironment Environment;
-        private readonly LoginHelper _loginHelper;
-        private readonly NavigationHelper _navigationHelper;
-        private readonly GroupHelper _groupHelper;
-        private readonly ContactHelper _contactHelper;
-        private FirefoxDriverFactory _driverFactory;
 
         public ApplicationManager()
         {
-            _driverFactory = new FirefoxDriverFactory();
-            _driver = _driverFactory.GetFirefoxDriver();
+            FirefoxDriverFactory driverFactory = new FirefoxDriverFactory();
+            Driver = driverFactory.GetFirefoxDriver();
 
-            _accountFactory = new AccountFactory();
+            AccountFactory = new AccountFactory();
             Environment = new TestingEnvironment();
-            _loginHelper = new LoginHelper(Driver);
-            _navigationHelper = new NavigationHelper(Driver, Environment);
-            _groupHelper = new GroupHelper(Driver);
-            _contactHelper = new ContactHelper(Driver);
+            LoginHelper = new LoginHelper(Driver);
+            NavigationHelper = new NavigationHelper(Driver, Environment);
+            GroupHelper = new GroupHelper(Driver);
+            ContactHelper = new ContactHelper(Driver);
 
         }
 
-        public NavigationHelper NavigationHelper => _navigationHelper;
-        public LoginHelper LoginHelper => _loginHelper;
-        public GroupHelper GroupHelper => _groupHelper;
-        public ContactHelper ContactHelper => _contactHelper;
+        public NavigationHelper NavigationHelper { get; }
 
-        public AccountFactory AccountFactory => _accountFactory;
+        public LoginHelper LoginHelper { get; }
 
-        public FirefoxDriverFactory DriverFactory => _driverFactory;
-        public IWebDriver Driver => _driver;
+        public GroupHelper GroupHelper { get; }
+
+        public ContactHelper ContactHelper { get; }
+
+        public AccountFactory AccountFactory { get; }
+
+        public IWebDriver Driver { get; }
 
         public void Stop()
 
