7f849dbe980dfa2fe8492cd15a94173558b2f12c
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2017-09-10 13:28:57 +0300

    Rewritten tests to use a fluent interface

--- csharp-test-training/csharp-test-training/Tests/ContactTest.cs
+++ csharp-test-training/csharp-test-training/Tests/ContactTest.cs
@@ -4,22 +4,25 @@ namespace addressbook_web_tests
 {
     public class ContactTest : TestBase
     {
-
         [Test]
         public void CreateContactTest()
         {
             app.NavigationHelper.OpenMainPage();
-            app.LoginHelper.FillLoginForm(app.AccountFactory.GetAdminAccountData());
-            app.LoginHelper.SubmitLoginForm();
+            app.LoginHelper.FillLoginForm(app.AccountFactory.GetAdminAccountData())
+                .SubmitLoginForm();
 
             app.NavigationHelper.GoToContactCreationPage();
-            ContactData data = new ContactData("TestName");
-            data.MiddleName = "TestMiddleName";
-            data.LastName = "TestLastName";
-            app.ContactHelper.FillContactForm(data);
-            app.ContactHelper.SubmitContactData();
-            app.LoginHelper.Logout();
 
+            ContactData data = new ContactData("TestName")
+            {
+                MiddleName = "TestMiddleName",
+                LastName = "TestLastName"
+            };
+
+            app.ContactHelper.FillContactForm(data)
+                .SubmitContactData();
+
+            app.LoginHelper.Logout();
         }
     }
 }
--- csharp-test-training/csharp-test-training/Tests/GroupTest.cs
+++ csharp-test-training/csharp-test-training/Tests/GroupTest.cs
@@ -5,20 +5,21 @@ namespace addressbook_web_tests
     [TestFixture]
     public class GroupTest : TestBase
     {
-
         [Test]
         public void TheUntitledTest()
         {
             app.NavigationHelper.OpenMainPage();
-            app.LoginHelper.FillLoginForm(app.AccountFactory.GetAdminAccountData());
-            app.LoginHelper.SubmitLoginForm();
-            app.NavigationHelper.GoToGroupsPage();
-            app.NavigationHelper.OpenCreateGroupPage();
-            GroupData data = new GroupData("Some new goup");
-            data.GroupHeader = "Some group header";
-            data.GroupFooter = "Некоторый русский текст для разнообразия.";
-            app.GroupHelper.FillGroupForm(data);
-            app.GroupHelper.SubmitGroupForm();
+            app.LoginHelper.FillLoginForm(app.AccountFactory.GetAdminAccountData()).
+                SubmitLoginForm();
+            app.NavigationHelper.GoToGroupsPage()
+                .OpenCreateGroupPage();
+            GroupData data = new GroupData("Some new goup")
+            {
+                GroupHeader = "Some group header",
+                GroupFooter = "Некоторый русский текст для разнообразия."
+            };
+            app.GroupHelper.FillGroupForm(data)
+                .SubmitGroupForm();
             app.LoginHelper.Logout();
         }
     }
